fd97e3a52fcc32ac42e0a07457d38ea449a076ed
Sergey Zarouski <serge.zarouski@gmail.com>
2018-11-26 18:08:31 -0500

    Add list icon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import './App.scss';
-import ListItem from "./components/ListItem/ListItem";
+import ListItem from './components/ListItem/ListItem';
+import ListItemIcon from './components/ListItem/ListItemIcon';
+import { ReactComponent as ListSVG } from './components/icons/list-icon.svg';
 
 class App extends Component {
   render() {
     return <ListItem>
+      <ListItemIcon>
+        <ListSVG />
+      </ListItemIcon>
       Hello
     </ListItem>;
   }
---
+++ src/components/ListItem/ListItemIcon.js
@@ -0,0 +1,10 @@
+import React, {PureComponent} from 'react';
+
+export default class ListItem extends PureComponent {
+  render() {
+    const {children} = this.props;
+    return <span className={'list-item__icon'}>
+      {children}
+    </span>;
+  }
+};
