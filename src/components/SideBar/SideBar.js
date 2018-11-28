098e32c971a0500ced13a4195bc5df1ce42354ed
Sergey Zarouski <serge.zarouski@gmail.com>
2018-11-27 21:56:19 -0500

    Add sidebar

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -1,31 +1,20 @@
-import React, {PureComponent, Fragment} from 'react';
-import PropTypes from 'prop-types';
+import React, {PureComponent} from 'react';
 import {connect} from "react-redux";
+import LaneFilter from "../LaneFilter/LaneFilter";
+import {Route, withRouter} from "react-router-dom";
 
 class SideBar extends PureComponent {
   render() {
-    const {laneData} = this.props;
-    return <div className={'side-bar'}>
-      <div className={'lane-filter'}>
-        {laneData.map(({year, status, bidType, type, lane, remaining, watching}) =>
-          <div className={`lane-filter__entry--${status}`}>
-            <div>
-              LANE {lane} {bidType}
-            </div>
-            <div className={'lane-filter__car-info'}>{year} {type} is live</div>
-            <div>
-              <span className={'lane-filter__remaining'}>{remaining} Items remaining</span>
-              {watching > 0 && <Fragment>
-                , <span className={'lane-filter__watching'}>{watching} {watching === 1 ? 'is' : 'are'} on your watchlist</span>
-              </Fragment>}
-            </div>
-          </div>)}
-      </div>
-    </div>;
+    const {laneData, filterTypes} = this.props;
+    return <Route path="/buy/live-lanes" exact={true} component={() => {
+      return <div className={'side-bar'}>
+        <LaneFilter {...{laneData, filterTypes}} />
+      </div>;
+    }}/>;
   }
 };
 SideBar.propTypes = {};
 
-export default connect(
-  ({laneData}) => ({laneData})
-)(SideBar);
+export default withRouter(connect(
+  ({laneData, filterTypes}) => ({laneData, filterTypes})
+)(SideBar));
