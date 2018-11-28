0c0314c846aa8bb36682b4bcba4ce069deecc57b
Sergey Zarouski <serge.zarouski@gmail.com>
2018-11-27 22:08:50 -0500

    Updates

---
+++ precache-manifest.d186e6b03b8934c316c03bf2655bfed3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e857af364d178b3cf04d7bef3d56ab0",
+    "url": "/sleepy-elbakyan/static/media/search.1e857af3.svg"
+  },
+  {
+    "revision": "f45999e0380f344650ac535a7a00e6a9",
+    "url": "/sleepy-elbakyan/static/media/ProximaNova-Semibold.f45999e0.woff"
+  },
+  {
+    "revision": "b7631b7639d1f6b1460a9f475ec89e8b",
+    "url": "/sleepy-elbakyan/static/media/ProximaNova-Semibold.b7631b76.woff2"
+  },
+  {
+    "revision": "1fa80a9ae0a07b03bca21dfab40dcb53",
+    "url": "/sleepy-elbakyan/static/media/ProximaNova-Regular.1fa80a9a.woff2"
+  },
+  {
+    "revision": "0930c99aa3403e19f3793d533bb677fa",
+    "url": "/sleepy-elbakyan/static/media/ProximaNova-Regular.0930c99a.woff"
+  },
+  {
+    "revision": "528c543f14c62813d71f",
+    "url": "/sleepy-elbakyan/static/js/runtime~main.528c543f.js"
+  },
+  {
+    "revision": "05c64cef40b5c09640d6",
+    "url": "/sleepy-elbakyan/static/js/main.05c64cef.chunk.js"
+  },
+  {
+    "revision": "af1053066234dffb3502",
+    "url": "/sleepy-elbakyan/static/js/1.af105306.chunk.js"
+  },
+  {
+    "revision": "05c64cef40b5c09640d6",
+    "url": "/sleepy-elbakyan/static/css/main.2261b45a.chunk.css"
+  },
+  {
+    "revision": "88bccf4f0606589154d5c87b5dba9bc0",
+    "url": "/sleepy-elbakyan/index.html"
+  }
+];
